3817097868fd5c96d2f25c52b0b17789577d2fd8
ysyonline163 <ysyonline@163.com>
2017-08-06 20:34:11 +0800

    list useage in webpack

--- app/components/ListControl/ListControl.js
+++ app/components/ListControl/ListControl.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import ListItem from './ListItem.js';
 
 class ListControl extends React.Component{
 
 	render(){
-		return{
-			
-		}
+		let list = this.props.datas;
+
+		const listItems = list.map(item => {
+			return <ListItem item={item} />
+		});
+
+		return(
+			<ul>{listItems}</ul>
+		);
 	}
 }
 
--- app/main.js
+++ app/main.js
@@ -6,18 +6,20 @@ document.getElementById("root").appendChild(greet());*/
 import React from 'react';
 import {render} from 'react-dom';
 
-import Greeter from './components/Greeter/Greeter.js';
-import './css/main.css'
+import listdatas from 'list';
+import './css/main.css';
 
+import Greeter from './components/Greeter/Greeter.js';
 import LoginControl from './components/LoginControl/LoginControl.js';
 import FormControl from './components/FormTest/FormControl.js';
-
+import ListControl from './components/ListControl/ListControl.js';
 
 render(
 	<div>
 		<Greeter />
 		<LoginControl/>
 		<FormControl />
+		<ListControl datas={} />
 	</div>
 	,
 	document.getElementById("root")
